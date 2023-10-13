5589e77c97da1d9e53621064874e44ce7ac40284
Tshiamo Komane <ST10122517@vcconnect.edu.za>
2023-10-13 14:27:44 +0200

    Add project files.

---
+++ Program/CityHierarchy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class CityHierarchy
+    {
+        private Dictionary<string, string> hierarchy = new Dictionary<string, string>();
+
+        public void Add(string child, string parent)
+        {
+            hierarchy[child] = parent;
+        }
+
+        public void FindParent(string child)
+        {
+            if (hierarchy.ContainsKey(child))
+            {
+                string parent = hierarchy[child];
+                Console.WriteLine(child);
+                if (parent != "-1")
+                {
+                    FindParent(parent);
+                }
+            }
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CityHierarchy cityHierarchy = new CityHierarchy();
+
+            // Add entries to the dictionary.
+            cityHierarchy.Add("South Africa", "-1");
+            cityHierarchy.Add("Gauteng", "South Africa");
+            cityHierarchy.Add("Pretoria", "Gauteng");
+            cityHierarchy.Add("Cape Town", "Western Cape");
+            cityHierarchy.Add("Johannesburg", "Gauteng");
+            cityHierarchy.Add("Western Cape", "South Africa");
+            cityHierarchy.Add("Durban", "KwaZulu-Natal");
+            cityHierarchy.Add("KwaZulu-Natal", "South Africa");
+
+            // Sample input and expected output.
+            string inputCity = "Gauteng";
+            Console.WriteLine("Input: " + inputCity);
+            cityHierarchy.FindParent(inputCity);
+        }
+    }
+}
